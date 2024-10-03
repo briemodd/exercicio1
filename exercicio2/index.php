870de9eddb5690a8d797cd7e360c9826a50251a4
briemodd <bibiairam1@gmail.com>
2024-10-03 12:12:11 -0300

    exercicio2

---
+++ exercicio2/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    $datetime = new DateTime( "now", new DateTimeZone( "America/Sao_Paulo" ) );
+    $hora = $datetime->format( 'H' );
+
+    if ($hora >= 0 && $hora <= 12) {
+        echo "<h1>Bom dia!</h1>";
+        echo "<img src='bomdia.jpg' alt='Bom dia!'>";
+        
+    } elseif ($hora > 12 && $hora < 18) {
+        echo "<h1>Boa tarde!</h1>";
+        echo "<img src='boatarde.jpg' alt='Boa tarde!'>";
+        
+    } else {
+        echo "<h1>Boa noite!</h1>";
+        echo "<img src='boanoite.jpg' alt='Boa noite!'>";
+    }
+        
+    
+
+    ?>
+</body>
+</html>
